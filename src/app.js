73ffb857f748aeee885bb024d370273da167e380
ANSON <anson11sd@gmail.com>
2024-10-23 22:24:14 +0530

    feat: crud api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const { default: mongoose } = require("mongoose");
+const { default: mongoose, model } = require("mongoose");
+const user = require("./models/user");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,14 +10,60 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    console.log("its coming here",req.body)
+  console.log("its coming here", req.body);
   const user = new User(req.body);
 
   await user.save();
   res.send("User added to DB successfully");
 });
 
+app.get("/getFeed", async (req, res) => {
+  console.log("entering feed")
+  try {
+    const allUsers = await User.find({});
+    if (allUsers.length) {
+      res.send(allUsers);
+    } else {
+      res.status(404).send("No user Data found");
+    }
+  } catch (error) {
+    res.status(400).send("Somthing went wtrong");
+  }
+});
 
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: email });
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Somthing went wtrong");
+  }
+});
+
+app.delete('/deleteUser', async (req,res) =>{
+  try {
+    const userId = req.body.userId;
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully")
+  } catch (error) {
+    res.status(400).send("Somthing went wtrong");
+    
+  }
+});
+
+app.patch('/update', async (req,res) =>{
+  const data = req.body;
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndUpdate({_id :userId},data);
+    res.send("User updated")
+  } catch (error) {
+    res.status(400).send("Somthing went wtrong");
+    
+  }
+})
 
 connectDB().then(() => {
   try {
