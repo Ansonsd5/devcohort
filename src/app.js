e54acfeb89ea4120c9eb9f69061599896a87ec20
ANSON <anson11sd@gmail.com>
2024-10-25 00:24:11 +0530

    feat: update email on id

--- src/app.js
+++ src/app.js
@@ -12,9 +12,14 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   console.log("its coming here", req.body);
   const user = new User(req.body);
-
+try {
   await user.save();
   res.send("User added to DB successfully");
+} catch (error) {
+  console.log("errr",error.message)
+  res.status(500).send(`Somthing went wrong ${error.message}`)
+}
+
 });
 
 app.get("/getFeed", async (req, res) => {
@@ -56,10 +61,10 @@ app.patch("/update", async (req, res) => {
   const userId = req.body.userId;
 
   try {
-    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    const user = await User.findByIdAndUpdate({ _id: userId }, data,{runValidators:true,returnDocument: 'after'});
     res.send("User updated");
   } catch (error) {
-    res.status(400).send("Somthing went wtrong");
+    res.status(400).send("Somthing went wtrong"+error.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  firstName: { type: String },
+  firstName: { type: String, required: true },
   lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
+  emailId: { type: String, required: true,unique:true },
+  password: { type: String, required: true },
   age: { type: Number },
   gender: {
     type: String,
+    validate(value){
+      if(!['male','female','others'].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    }
   },
+  skill :[String]
 });
 
-module.exports  = mongoose.model("User",userSchema);
-
-
+module.exports = mongoose.model("User", userSchema);
