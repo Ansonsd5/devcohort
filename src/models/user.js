0fa669d5d4add1bbba85a4009cb5d2e9334df64a
ANSON <anson11sd@gmail.com>
2024-10-25 22:04:24 +0530

    feat: usage of validator package

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
-
+const { trim } = require("validator");
+const validator = require("validator");
 const userSchema = mongoose.Schema(
   {
     firstName: { type: String, required: true, minLength: 4, maxLength: 40 },
     lastName: { type: String },
-    emailId: { type: String, required: true, unique: true },
+    emailId: { 
+      type: String, 
+      lowercase:true,
+      trim:true,
+      required: true, 
+      unique: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error(`Invalid Email Address ${value}`)
+        }
+      }
+    },
     password: { type: String, required: true },
     age: { type: Number },
     gender: {
